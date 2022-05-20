fec78f85af06f0e8840292aa531b63e095b21290
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-05-20 11:47:27 +0200

    Add effect and media queries ton navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,35 @@
 import React from "react";
+import { useState } from "react";
 import "../components/Navbar.css";
 import logoblanc from "../assets/logoblanc.png"
 
 function Topbar() {
   
+    const [showLinks, setShowLinks] =useState(false)
+    const handleShowLinks = () => {
+        setShowLinks(!showLinks)
+    }
+
   return (
-   <nav className="navbar">
-       <div className={logoblanc}></div>
+   <nav className={`navbar ${showLinks? "show-nav":"hide-nav"}`}>
+       <div className="logo"><a href="#presentation"><img src={logoblanc} alt="Logo de Wendy Baqué" className="logo"></img></a></div>
        <ul className="navbar-links">
-        <li className="navbar-item">
+        <li className="navbar-item slideInDown-1">
             <a href="/" className="navbar-link">Accueil</a>
         </li>
-        <li className="navbar-item">
+        <li className="navbar-item slideInDown-2">
             <a href="#presentation" className="navbar-link">Présentation</a>
         </li>
-        <li className="navbar-item">
+        <li className="navbar-item slideInDown-3">
             <a href="#livre" className="navbar-link">Mes livres</a>
         </li>
-        <li className="navbar-item">
+        <li className="navbar-item slideInDown-4">
             <a href="#contact" className="navbar-link">Contact</a>
         </li>
        </ul>
+       <button className="navbar-burger" onClick={handleShowLinks}>
+           <span className="burger-line"></span>
+       </button>
    </nav>
   );
 }
