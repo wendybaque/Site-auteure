5b8d1041df86f8d32df46022d08a927a5631e6ba
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-05-21 15:05:18 +0200

    Add animation in presentation component

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -2,8 +2,20 @@ import React from "react";
 import "../components/Presentation.css";
 import logo from "../assets/logoblanc.png"
 import down from "../assets/down.png"
+import { init } from "ityped"
+import { useEffect, useRef } from "react";
 
 function Presentation () {
+    const textRef=useRef();
+    useEffect(()=>{
+        init(textRef.current, {
+            backDelay:1500,
+            backSpeed:40, 
+            showCursor: true,
+            strings: ["romans contemporains","romans feel-good", "romances musicales" ]
+        });
+    },[])
+
     return (
         <div className="presentation" id="presentation">
         <div className="presentation-left">
@@ -15,7 +27,7 @@ function Presentation () {
             <div className="presentation-wrapper">
                 <h2>Bonjour et bienvenue sur mon site ! Je suis</h2>
                 <h1>Wendy Baqué</h1>
-                <h3>Auteure de <span>romans contemporains </span>, feel-good et de romances musicales</h3>
+                <h3>Auteure de <span ref={textRef} className="presentation-span"></span></h3>
 
             <a href="#livre" >
                 <img src={down} alt="flèche vers le bas" className="arrow"></img>
