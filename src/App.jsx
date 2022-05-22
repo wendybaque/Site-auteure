1b5b3610470812c3c8c3bd8f40bd8ac72831c87c
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-05-22 10:39:46 +0200

    Remove animation in presentation section and change text

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import "./App.css";
+
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Accueil from "./pages/Accueil";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,21 +1,10 @@
 import React from "react";
 import "../components/Presentation.css";
-import logo from "../assets/logoblanc.png"
+import logo from "../assets/logonoir.png"
 import down from "../assets/down.png"
-import { init } from "ityped"
-import { useEffect, useRef } from "react";
 
 function Presentation () {
-    const textRef=useRef();
-    useEffect(()=>{
-        init(textRef.current, {
-            backDelay:1500,
-            backSpeed:40, 
-            showCursor: true,
-            strings: ["romans contemporains","romans feel-good", "romances musicales" ]
-        });
-    },[])
-
+    
     return (
         <div className="presentation" id="presentation">
         <div className="presentation-left">
@@ -25,14 +14,10 @@ function Presentation () {
         </div>
         <div className="presentation-right">
             <div className="presentation-wrapper">
-                <h2>Bonjour et bienvenue sur mon site ! Je suis</h2>
+                <h3>Bonjour ! Je suis</h3>
                 <h1>Wendy Baqué</h1>
-                <h3>Auteure de <span ref={textRef} className="presentation-span"></span></h3>
-                <p>Connue sous le pseudo d'Antiigone, Wendy Baqué est une auteure qui adore jouer avec les mots.</p>
-                <p>Pour elle, écrire a toujours été un moyen de communication à part entière : tout ce qui ne peut se dire peut s'écrire. C'est pour cela que l'expression des émotions de ses personnages prend une grande place dans ses écrits, avec une recherche particulière apportée au réalisme.</p>                <p>"Les dangers de la lumière", une romance musicale dans la France des années 80 et placée sous le signe du rock and roll, est éditée en août 2021, également chez Plumes de Mimi éditions.</p>
-                <p>De manière générale, ses romans traitent de sujets qui lui sont chers, mais aussi d'actualité, tantôt tabous, tantôt victimes de préjugés et de stéréotypes, comme la fin de vie, l'avortement, l'acceptation de soi, la santé mentale ou encore les normes de la société. Elle porte une attention particulière aux émotions.</p>
-                <p>Ses personnages ne sont pas parfaits, ont des défauts, des failles à travers lesquelles ils laissent entrer ou jaillir la lumière, ce qui les rend d'autant plus humains. Sa deuxième passion, la musique, tient une grande place dans ses écrits.Susciter la réflexion et le questionnement de soi chez ses lecteurs est un point clef de la fonction de l'écrivain, selon elle.</p>
-                <p>Elle réalise aussi des chroniques littéraires de romans auto-édités ou de maison d'édition indépendantes, afin de mettre en avant de nouveaux auteurs et ses coups de cœur littéraires que vous pouvez retrouver sur <a href="https://wendybaqueauteur.wixsite.com/website" className="site-de-chroniqueuse">ce site</a></p>
+                <h3>Auteure de </h3>
+                <p>Bienvue sur mon site ! Vous y découvrirez mon univers littéraire, fait de récits contemporains et réalistes. J'ai à coeur de traiter de sujets originaux à trvaers des personnages humains et attachants. Je vous souhaite une bonne visite et ce belles lectures !</p>
             <a href="#livre" className="redirection">
                 <img src={down} alt="flèche vers le bas" className="arrow"></img>
             </a>
