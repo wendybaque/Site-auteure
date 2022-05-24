bc89c1745b34a7efe678091b7849348a2d656ac7
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-05-24 13:41:03 +0200

    Install react awesome package and try it in presentation component

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import "../components/Presentation.css";
-import logo from "../assets/logonoir.png"
-import down from "../assets/down.png"
+import logo from "../assets/logonoir.png";
+import down from "../assets/down.png";
+import { Fade } from "react-awesome-reveal";
+
+
 
 function Presentation () {
     
@@ -17,6 +20,15 @@ function Presentation () {
                 <h3>Bonjour ! Je suis</h3>
                 <h1>Wendy Baqué</h1>
                 <h3>Auteure de </h3>
+                <Fade delay={1000}> 
+                <p className="fade">Romans contemporains</p>
+                </Fade>
+                <Fade delay={2000}>
+                <p className="fade">Romans feel-good</p>
+                </Fade>
+                <Fade delay={3000}>
+                <p className="fade">Romances musicales</p>
+                </Fade>
                 <p>Bienvue sur mon site ! Vous y découvrirez mon univers littéraire, fait de récits contemporains et réalistes, avec quelques notes de romance. J'ai à coeur de traiter de sujets originaux à trvaers des personnages humains et attachants. Je vous souhaite une bonne visite et ce belles lectures !</p>
                 <p>Mes livres sont disponibles sur Amazon en versions numériques et imprimées, ainsi qu'à la commande dans vos librairies en ligne et physiques favorites.</p>
             <a href="#livre" className="redirection">
