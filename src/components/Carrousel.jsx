4f24247253d81c2053ccaad944772d77afcac1bc
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-05-28 13:29:54 +0200

    Add 1 part of carrousel component

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import "../components/Carrousel.css";
+import livredol from "../assets/livredol.png";
 
 function Carrousel () {
     return (
         <div className="carrousel" id="carrousel">
-        <h2>CARROUSEL</h2>
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="item-left">
+                            <div className="left-container">
+                                <h2 className="carrousel-title">Les dangers de la lumière</h2>
+                                <p>Genre : romance musicale</p>
+                                <a href="zeqsdxf" alt="esdrfg" ><button>En savoir plus</button></a>
+                            </div>
+                        </div>
+                        <div className="item-right">
+                        <img src={livredol} alt="Couverture des dangers de la lumière" className="carrousel-img"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
