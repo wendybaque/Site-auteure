9e473ca6a1824873d8007fd1966f19b6185ff12f
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-07-07 08:32:46 +0200

    Change color and transition duration in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,30 +12,30 @@ function Topbar() {
     <a href="/" alt="Back home"><span class="font-semibold font-cursive text-xl tracking-tight m-2">Wendy Baqué</span></a>
   </div>
   <div class="block lg:hidden">
-    <button class="flex items-center px-3 py-2 border rounded text-white border-sky-700 hover:text-white hover:border-white">
+    <button class="flex items-center px-3 py-2 border rounded text-white border-sky-700 hover:text-white hover:border-white transition duration-300">
       <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
     </button>
   </div>
   <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
     <div class="text-sm lg:flex-grow">
-      <a href="/Apropos" className="text-base font-bold font-poppins p-2 block mt-4 lg:inline-block lg:mt-0 text-white hover:text-sky-700 mr-4">
+      <a href="/Apropos" className="text-base font-bold font-poppins p-2 block mt-4 lg:inline-block lg:mt-0 text-white hover:text-cyan-100 mr-4 transition duration-300">
         A propos
       </a>
-      <a href="/Dangersdelalumiere" className="text-base font-bold font-poppins p-2 block mt-4 lg:inline-block lg:mt-0 text-white hover:text-sky-700 mr-4">
+      <a href="/Dangersdelalumiere" className="text-base font-bold font-poppins p-2 block mt-4 lg:inline-block lg:mt-0 text-white hover:text-cyan-100 mr-4 transition duration-300">
         Les dangers de la lumière
       </a>
-      <a href="/Inoubliablesymphonie" className="text-base font-bold font-poppins p-2 block mt-4 lg:inline-block lg:mt-0 text-white hover:text-sky-700">
+      <a href="/Inoubliablesymphonie" className="text-base font-bold font-poppins p-2 block mt-4 lg:inline-block lg:mt-0 text-white hover:text-cyan-100 transition duration-300">
         Inoubliable Symphonie
       </a>
-      <a href="/Ventsdelexistence" className="text-base font-bold font-poppins p-2 block mt-4 lg:inline-block lg:mt-0 text-white hover:text-sky-700">
+      <a href="/Ventsdelexistence" className="text-base font-bold font-poppins p-2 block mt-4 lg:inline-block lg:mt-0 text-white hover:text-cyan-100 transition duration-300">
         Les vents de l'existence
       </a>
-      <a href="/Fleurdelage" className="text-base font-bold font-poppins p-2 block mt-4 lg:inline-block lg:mt-0 text-white hover:text-sky-700">
+      <a href="/Fleurdelage" className="text-base font-bold font-poppins p-2 block mt-4 lg:inline-block lg:mt-0 text-white hover:text-cyan-100 transition duration-300">
         La fleur de l'âge
       </a>
     </div>
     <div>
-      <a href="/Contact" class="text-base font-bold font-poppins inline-block px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-sky-700 hover:bg-white mt-4 lg:mt-0">Contact</a>
+      <a href="/Contact" class="text-base font-bold font-poppins inline-block px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-sky-700 hover:bg-white mt-4 lg:mt-0 transition duration-300">Contact</a>
     </div>
   </div>
 </nav>
